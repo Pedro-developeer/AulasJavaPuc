73b88dd851e97ae36f56b87d4bbc7225714e8feb
Pedro Henrique <79731855+Pedro-developeer@users.noreply.github.com>
2021-03-21 14:55:20 -0300

    Update Lista1Att4.java

--- Lista1Att4.java
+++ Lista1Att4.java
@@ -6,23 +6,33 @@ O custo do seguro contra granizo numa comunidade típica de fazendeiros  é 3,5%
  o custo de seguro para cada produto e o custo total do seguro para o cliente.
  */
 import java.util.Scanner;
-public class Lista1Exercicio04 {
-    public static
-    void main(String[] args) {
-        double milho,feijao,cafe;
-        double custoTotal,custo1,custo2,custo3;
+public class Lista1Att4 {
+    public static void main(String[] args) {
+    double preçoMilho,preçoFeijao,preçoCafe,seguroMilho,seguroFeijao,seguroCafe,seguro;
+    double acreMilho,acreFeijao,acreCafe;
         Scanner ler = new Scanner(System.in);
-        System.out.println("informe o valor de milho : ");
-        milho=ler.nextDouble();
-        System.out.println("informe o valor de feijão : ");
-        feijao=ler.nextDouble();
-        System.out.println("informe o valor de café: ");
-        cafe=ler.nextDouble();
-
-        custo1= milho * 3.5 / 100;
-        custo2= feijao * 3.5 / 100;
-        custo3= cafe * 3.5 / 100;
-        custoTotal=custo1+custo2+custo3;
-        System.out.println(custoTotal);
+        System.out.println("Informe a quantidade de acres para milho:");
+        acreMilho= ler.nextFloat();
+        System.out.println("Informe a quantidade de acre de feijão:");
+        acreFeijao= ler.nextFloat();
+        System.out.println("Informe a quantidade de acre de cafe:");
+        acreCafe= ler.nextFloat();
+        System.out.println("Informe o valor de acres para milho:");
+        preçoMilho= ler.nextDouble();
+        System.out.println("Informe o valor de acres para feijão:");
+        preçoFeijao= ler.nextDouble();
+        System.out.println("Informe o valor de acres de café:");
+        preçoCafe= ler.nextDouble();
+        seguroMilho=0.035*(acreMilho*preçoMilho);
+        seguroFeijao=0.035*(acreFeijao*preçoFeijao);
+        seguroCafe=0.035*(acreCafe*preçoCafe);
+        seguro=seguroMilho+seguroCafe+seguroFeijao;
+        System.out.println("O valor do seguro do milho R$"+seguroMilho);
+        System.out.println("O valor do seguro do feijão R$"+seguroFeijao);
+        System.out.println("O valor do seguro do feijão R$"+seguroCafe);
+        System.out.println("O valor total do seguro é R$:" +seguro);
+        
+    }
+}
 
     }
