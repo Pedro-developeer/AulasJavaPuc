145dadcb3e19875fb0805126351099e096adb36c
Pedro Henrique <79731855+Pedro-developeer@users.noreply.github.com>
2021-05-15 20:18:39 -0400

    Add files via upload

---
+++ Lista5Att8.java
@@ -0,0 +1,16 @@
+package Lista5;
+
+import java.util.Scanner;
+
+/*Faça um programa  para  ler  o  sexo  (1-masculino,  2-feminino)  e  a  altura de  um  grupo  de  50  pessoas.
+O programa deve calcular :
+a altura média das mulheres e a altura média dos homens;
+a maior e a menor altura do grupo, dizendo se a altura é de um homem ou de uma mulher.
+
+ */
+public class Lista5Att8 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        
+    }
+}
